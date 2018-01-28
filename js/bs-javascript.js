c2f7d73968f0093ba84791e6e96eabece31faf55
Britt Scott <stacalkas_nomen@hotmail.com>
2018-01-28 14:36:17 -0600

    Removed google's ReCAPTCHA. Reverted to using old captcha system. Changed to version 4.0.2.

--- js/bs-javascript.js
+++ js/bs-javascript.js
@@ -12,17 +12,17 @@ function startup(){
 // Dynamic Copyright Year //
 ////////////////////////////
 function getYear(){
-  var today = new Date();
-  var currentYear = today.getFullYear();
-  document.getElementsByName("dynYear")[0].innerText = currentYear;
-  document.getElementsByName("dynYear")[1].innerText = currentYear;
-  clearTimeout(getYear); // Kill our setTimeout to stop calling this
+     var today = new Date();
+     var currentYear = today.getFullYear();
+     document.getElementsByName("dynYear")[0].innerText = currentYear;
+     document.getElementsByName("dynYear")[1].innerText = currentYear;
+     clearTimeout(getYear); // Kill our setTimeout to stop calling this
 }
 // Site Version //
 //////////////////
 function siteVersion(){
-  document.getElementById("siteVer").innerHTML = "4.0.1";
-  clearTimeout(siteVersion); // Kill our setTimeout to stop calling this
+     document.getElementById("siteVer").innerHTML = "4.0.2";
+      clearTimeout(siteVersion); // Kill our setTimeout to stop calling this
 }
 // Mobile menu show/hide //
 ///////////////////////////
@@ -44,76 +44,87 @@ function getLastMod(){
 }
 // on submit btn click, check each input //
 ///////////////////////////////////////////
-$("#submit").click(function()
-{
-     if(!validateText("name"))
-     {
-          return false;
-     }
-     if(!validateText("email"))
-     {
-          return false;
-     }
-     // This is optional, so don't validate
-     // if(!validateText("telephone"))
-     // {
-          // return false;
-     // }
-     if(!validateText("message"))
-     {
-          return false;
-     }
+// $("#submit").click(function()
+// {
+//      if(!validateText("name"))
+//      {
+//           return false;
+//      }
+//      if(!validateText("email"))
+//      {
+//           return false;
+//      }
+//      // This is optional, so don't validate
+//      // if(!validateText("telephone"))
+//      // {
+//           // return false;
+//      // }
+//      if(!validateText("message"))
+//      {
+//           return false;
+//      }
 
-     //This is where we check to see if the captcha has
-     //been triggered to decide if, is human.
-     var v = grecaptcha.getResponse();
-     if(v.length == 0)
-     {
-          $(".captcha").show();
-          return false;
-     }
-     else
-     {
-          $(".captcha").hide();
-          return true;
-     }
-});
+//      //This is where we check to see if the captcha has
+//      //been triggered to decide if, is human.
+     
+//      // var v = grecaptcha.getResponse();
+//      // if(v.length == 0)
+//      // {
+//      //      $(".captcha").show();
+//      //      return false;
+//      // }
+//      // else
+//      // {
+//      //      $(".captcha").hide();
+//      //      return true;
+//      // }
+// });
 // Display form alert after submit //
 /////////////////////////////////////
 $("#thisForm").submit(function(){
-     $(".error").hide(); //Clear error message
-     $(".captcha").hide();
+     //$(".error").hide(); //Clear error message
+     //$(".captcha").hide();
      $(".success").show();
      $(".success").fadeOut(5000);
-     //$("#captcha").attr("src", "php/captcha.php");
-     //$("#captcha").removeAttr("src").attr("src", "php/captcha.php");
+     $(".captchaImg").attr("src", ""); // Clear captcha source, so we can reload it later
+     //$(".captchaImg").attr("src", "php/captcha.php");
+     //$(".captchaImg").removeAttr("src").attr("src", "php/captcha.php");
      //event.target.reset(); //this clears the inputs to their default placeholders
      //setTimeout(reloadPage, 3000); 	// Relaod the page after Message status is displayed
                                         // this prevents multiple, rapid messages being sent.
-     var form = document.getElementById("thisForm");                                   
-     form.reset();
+     setTimeout(resetForm, 100); 
+     //var form = document.getElementById("thisForm");                                 
+     //form.reset(); //Reset the form
+     //grecaptcha.reset(); //Reset the reCaptcha
 });
+
+function resetForm()
+{
+     var form = document.getElementById("thisForm");                                 
+     form.reset(); //Reset the form
+     $(".captchaImg").attr("src", "php/captcha.php"); // Reload Captcha
+}	
 // form client-side validation //
 /////////////////////////////////
-function validateText(id)
-{
-     if($("#"+id).val()==null || $("#"+id).val()=="")
-     {
-          var div = $("#"+id).closest("div");
-          //var div = $("#"+id).closest(".w3-rest");
-          div.addClass("bs-form-error");
-          $(".error").show();
-          //alert("Validation Error!!!");
-          return false;
-     }
-     else
-     {
-          var div = $("#"+id).closest("div");
-          //var div = $("#"+id).closest(".w3-rest");
-          div.removeClass("bs-form-error");
-          return true;
-     }
-}
+// function validateText(id)
+// {
+//      if($("#"+id).val()==null || $("#"+id).val()=="")
+//      {
+//           var div = $("#"+id).closest("div");
+//           //var div = $("#"+id).closest(".w3-rest");
+//           div.addClass("bs-form-error");
+//           $(".error").show();
+//           //alert("Validation Error!!!");
+//           return false;
+//      }
+//      else
+//      {
+//           var div = $("#"+id).closest("div");
+//           //var div = $("#"+id).closest(".w3-rest");
+//           div.removeClass("bs-form-error");
+//           return true;
+//      }
+// }
 // Clear form mesaages on clear btn click //
 ////////////////////////////////////////////
 $("#clear").click(function()
@@ -526,5 +537,3 @@ bs.displayObject = function (id, data) {
     a.innerHTML = a.innerHTML.replace(r, result);
   }
 };	
-	
-	
--- php/ProcessForm.php
+++ php/ProcessForm.php
@@ -1,52 +1,30 @@
 <?php
-if(isset($_POST['submit'])):
-    if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])):
-		//your secret key
-        $secret = '6LfFPRcUAAAAAItAoWDOePpWpq4n_eBoIAOoN5sf';
-		//get verify response data
-        $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
-        $responseData = json_decode($verifyResponse);
+     session_start();
 
-		$name = !empty($_POST['name'])?$_POST['name']:'';
-		$email = !empty($_POST['email'])?$_POST['email']:'';
-          $telephone = !empty($_POST['telephone'])?$_POST['telephone']:'';
-          $message = !empty($_POST['message'])?$_POST['message']:'';
-        if($responseData->success):
-			//contact form
-			$to = 'stacalkas_nomen@hotmail.com';
-			$subject = 'From Britts site';
-			$htmlContent = "
-				<h1>Britts Site Contact Form</h1>
-				<p><b>Name: </b>".$name."</p>
-				<p><b>Email: </b>".$email."</p>
-                <p><b>Telephone: </b>".$telephone."</p>
-				<p><b>Message: </b>".$message."</p>
-			";
-			// set content-type when sending an html email
-			$headers = "MIME-Version: 1.0" . "\r\n";
-			$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-			// More kinds of headers
-			$headers .= 'From:'.$name.' <'.$email.'>' . "\r\n";
-			//send the email
-			@mail($to,$subject,$htmlContent,$headers);
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $telephone = $_POST['telephone'];
+    $message = $_POST['comment'];
+	
+//$statusSent = $_POST['statusSent'];
+	
+    $from = 'From: Britts Site'; 
+    $to = 'stacalkas_nomen@hotmail.com'; 
+    $subject = 'Message from Britts Site';
+			
+    $body = "Name: $name\n Phone: $telephone\n E-Mail: $email\n Message:\n $message";
 
-            $succMsg = 'Your contact request have submitted successfully.';
-			$name = '';
-			$email = '';
-			$message = '';
-        else:
-			$errMsg = 'Robot verification failed, please try again.';
-               echo $errMsg;
-        endif;
-    else:
-		 $errMsg = 'Please click on the reCAPTCHA box.';
-           echo $errMsg;
-    endif;
-else:
-    $errMsg = '';
-    $succMsg = '';
-	$name = '';
-	$email = '';
-	$message = '';
-endif;
+    // uncomment to test without vercode
+    //mail ($to, $subject, $body, $from); 
+				
+    if ($_POST['submit'] && $_POST['vercode']==$_SESSION['vercode'] && $name != '' && $email != '' && $message != '') 
+	{				 
+        //$statusSent = 'Message, Sent';
+		
+		$mail_status = mail ($to, $subject, $body, $from); 
+	}
+	else 
+	{ 
+		//$statusSent = 'Message, was NOT Sent!!!'; 
+	}
 ?>
