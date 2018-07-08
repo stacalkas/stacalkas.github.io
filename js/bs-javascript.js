21011257b970014fe6dd6a439f81990bb68959d6
Britt Scott <stacalkas_nomen@hotmail.com>
2018-07-07 20:20:02 -0500

    Updated to Font Awesome 5.1.0 Updated "main" logo Updated download links on images.html to Google Drive Links

--- js/bs-javascript.js
+++ js/bs-javascript.js
@@ -21,7 +21,7 @@ function getYear(){
 // Site Version //
 //////////////////
 function siteVersion(){
-     document.getElementById("siteVer").innerHTML = "4.0.3";
+     document.getElementById("siteVer").innerHTML = "4.0.4";
       clearTimeout(siteVersion); // Kill our setTimeout to stop calling this
 }
 // Mobile menu show/hide //
